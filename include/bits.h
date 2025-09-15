90599e5bd538fd73822a05d04e33c37fdcb534bc
Tomas Baublys <tomas.baublys@mif.stud.vu.lt>
2025-09-15 16:59:45 +0300

    updated bits.h

--- include/bits.h
+++ include/bits.h
@@ -5,10 +5,13 @@ class Bits {
 	private:
 		uint64_t size;
 	public:
-		Bits(uint64_t &size) size(size) {
+		Bits(uint64_t &size): size(size) {
 		
 		}
 
+	uint64_t size() {
+		return this -> size;
+	}
 };
 
 #endif
