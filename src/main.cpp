39166a12712faef588c51895a4f91bc4807f0ad2
godagu <goda.gutparakyte@mif.stud.vu.lt>
2025-09-15 22:59:56 +0300

    hello guys

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 int main(int argc, char* argv[]) {
 	std::cout << "Sveikas pasauli, as Tomas\n";
+	std:ciut << "Nesveikas pasauli, as Tomas irgi\n";
 	\\ komandos nariu pasisveikinimai...
 
 	return 0;
