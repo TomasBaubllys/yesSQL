7c8523586db12af00287a07a76262646b10c988a
Tomas Baublys <tomas.baublys@mif.stud.vu.lt>
2025-09-18 10:59:55 +0300

    added immplemention for most of the bits class

--- include/bits.h
+++ include/bits.h
@@ -1,11 +1,14 @@
 #ifndef YSQL_BITS_H_INCLUDED
 #define YSQL_BITS_H_INCLUDED
 
+#define BIT_LEN_UPDATE_ERR "Bit stream lengths do not match!"
+
 #include <string>
 #include <algorithm>
 #include <cmath>
 #include <vector>
 #include <cstdint>
+#include <stdexcept>
 
 using bit_arr_size_type = std::vector<uint8_t>::size_type;
 
@@ -13,9 +16,17 @@ class Bits {
 	private:
 		std::vector<uint8_t> arr;
 	public:
-		// constructor, no default constructor exists
-		Bits(bit_arr_size_type &size);
+		// constructors, no default constructor exists
+		Bits(std::vector<uint8_t>& bitstream);
 	
+		Bits(std::vector<char>& bitstream);
+	
+		Bits(std::vector<uint32_t>& bitstream);
+		
+		Bits(std::string& bitstream);
+	
+		Bits(Bits& org);
+			
 		// desctructor
 		~Bits(); 
 
@@ -23,18 +34,22 @@ class Bits {
 		bit_arr_size_type size();
 		
 		// returns the bits as a character vector
-		std::vector<uint8_t> get_char_vector();
+		std::vector<char> get_char_vector();
 
 		// returns the bits as a string
 		std::string get_string_char();
 
 		// returns bits as a string containing only 0 and 1s
-		std::string get_string_bits();
+		// std::string get_string_bits();
 
 		// returns the bits as and integer vector, if bits dont fill the last integer it will be padded with 0 bits to be interpreted as little endian
 		std::vector<uint32_t> get_int_array();   
 		
+		// compares to bit objects bit by bit
 		inline bool operator==(const Bits& other);
+
+		// update the bitstream !throws!
+		void update_bits(std::vector<uint8_t>& new_bits);
 };
 
 #endif
