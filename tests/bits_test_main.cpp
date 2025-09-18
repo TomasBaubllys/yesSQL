220e32c3d28ba5eee8c92563d9c3c3f31c29acf0
Tomas Baublys <tomas.baublys@mif.stud.vu.lt>
2025-09-18 12:46:20 +0300

    added testing script

---
+++ tests/bits_test_main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cassert>
+#include "../include/bits.h"
+
+#ifdef BITS_TEST_MAIN
+
+int main(int argc, char* argv[]) {
+
+    std::cout << "Hello world!\n";
+    return 0;
+}
+
+#endif
