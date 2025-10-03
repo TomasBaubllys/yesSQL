26086a8e1cff5ccdb0eef6b888e22b25eab34b46
Tomas Baublys <tomas.baublys@mif.stud.vu.lt>
2025-10-03 10:00:29 +0300

    merged avl-tree files from avl-tree branch into feature/LSM-Tree

--- src/avl_tree.cpp
+++ src/avl_tree.cpp
@@ -1,13 +1,5 @@
 #include "../include/avl_tree.h"
 
-/*
-TODO:
-- when inserting check entry checksum
--
--
--
-*/
-
 AVL_tree::Node::Node(Entry& entry) : data(entry), left(nullptr), right(nullptr), height(1) {
 
 }
@@ -127,6 +119,68 @@ AVL_tree::Node* AVL_tree::min_value_node(AVL_tree::Node* node) {
 	return current;
 }
 
+AVL_tree::Node* AVL_tree::delete_node(AVL_tree::Node* node, Bits& key) {
+	if(!node) {
+		return node;
+	}
+
+	if(key < node -> data.get_key()) {
+		node -> left = this -> delete_node(node -> left, key);
+	}
+	else if(key > node -> data.get_key()) {
+		node -> right = this -> delete_node(node -> right, key);
+	}
+	else {
+		if(!(node -> left) || !(node -> right)) {
+			AVL_tree::Node* temp = node -> left? node -> left : node -> right;
+			if(!temp) {
+				// temp = node;
+				delete node;
+				node = nullptr;
+			}
+			else {
+				AVL_tree::Node* old = node;
+				node = temp;
+				delete old;
+			}
+		}
+		else {
+			AVL_tree::Node* temp = min_value_node(node -> right);
+			node -> data = temp -> data;
+			Bits temp_key = temp -> data.get_key();
+			node -> right = this -> delete_node(node -> right, temp_key);
+		}
+	}
+
+	if(!node) {
+		return node;
+	}
+
+	node -> height = 1 + std::max(this -> height(node -> left), this -> height(node -> right));
+
+	int32_t balance = this -> get_balance(node);
+
+	if(balance > 1 && this -> get_balance(node -> left) >= 0) {
+		return this -> right_rotate(node);
+	}
+
+	if(balance > 1 && this -> get_balance(node -> left) < 0) {
+		node -> left = this -> left_rotate(node -> left);
+		return this -> right_rotate(node);
+	}
+
+	if(balance < -1 && this -> get_balance(node -> right) <= 0) {
+		return this -> left_rotate(node);
+	}
+
+	if(balance < -1 && this -> get_balance(node -> right) > 0) {
+		node -> right = this -> right_rotate(node -> right);
+		return this -> left_rotate(node);
+	}
+
+	return node;
+}
+
 // instead of entry use Bits
 AVL_tree::Node* AVL_tree::delete_node(AVL_tree::Node* node, Entry& entry) {
 	if(!node) {
@@ -143,13 +197,15 @@ AVL_tree::Node* AVL_tree::delete_node(AVL_tree::Node* node, Entry& entry) {
 		if(!(node -> left) || !(node -> right)) {
 			AVL_tree::Node* temp = node -> left? node -> left : node -> right;
 			if(!temp) {
-				temp = node;
+				// temp = node;
+				delete node;
 				node = nullptr;
 			}
 			else {
-				*root = *temp;
+				AVL_tree::Node* old = node;
+				node = temp;
+				delete old;
 			}
-			delete temp;
 		}
 		else {
 			AVL_tree::Node* temp = min_value_node(node -> right);
@@ -180,7 +236,7 @@ AVL_tree::Node* AVL_tree::delete_node(AVL_tree::Node* node, Entry& entry) {
 	}
 
 	if(balance < -1 && this -> get_balance(node -> right) > 0) {
-		node -> right = this -> right_rotate(root -> right);
+		node -> right = this -> right_rotate(node -> right);
 		return this -> left_rotate(node);
 	}
 
@@ -248,6 +304,16 @@ void AVL_tree::remove(Entry& entry) {
 	this -> root = new_root;
 }
 
+void AVL_tree::remove(Bits& key) {
+	AVL_tree::Node* new_root = this -> delete_node(this -> root, key);
+	if(!new_root) {
+		std::cerr << AVL_TREE_DELETION_FAILED_ERR;
+		return;
+	}
+
+	this -> root = new_root;
+}
+
 Entry AVL_tree::search(Bits& key, bool& found) {
 	return this -> search(this -> root, key, found);
 }
@@ -258,4 +324,69 @@ std::vector<Entry> AVL_tree::inorder() {
 	return entry_vector;
 }
 
+void AVL_tree::make_empty(AVL_tree::Node* root) {
+	if(!root) {
+		return;
+	}
+	
+	this -> make_empty(root -> left);
+	this -> make_empty(root -> right);
 
+	delete root;
+	root = nullptr;
+} 
+
+void AVL_tree::make_empty() {
+	if(this -> root) {
+		this -> make_empty(this -> root);	
+	}
+}
+
+AVL_tree::~AVL_tree() {
+	this -> make_empty();
+}
+
+Entry AVL_tree::pop_last() {
+	return this -> pop_last(this -> root);
+}
+
+Entry AVL_tree::pop_last(AVL_tree::Node*& node) {
+	// if tree is empty return a placeholder
+	if(!node) {
+		std::string string_key(ENTRY_PLACEHOLDER_KEY);
+		std::string string_value(ENTRY_PLACEHOLDER_VALUE);
+		return Entry(Bits(string_key), Bits(string_value));
+	}
+	
+	// rightmost node
+	if(!(node -> right)) {
+		Entry result = node -> data;
+		AVL_tree::Node* left_child = node -> left;
+		delete node;
+		node = left_child;
+		return result;
+	}
+
+	Entry result = this -> pop_last(node -> right);
+	
+	node -> height = 1 + std::max(this -> height(node -> left), this -> height(node -> right));
+
+	int32_t balance = this -> get_balance(node);
+
+	if(balance > 1 && this -> get_balance(node -> left) >= 0) {
+		node = this -> right_rotate(node);
+	}
+	else if(balance > 1 && this -> get_balance(node -> left) < 0) {
+		node -> left = this -> left_rotate(node -> left);
+		node = this -> right_rotate(node);
+	}
+	else if(balance < -1 && this -> get_balance(node -> right) <= 0) {
+		node = this -> left_rotate(node);
+	}
+	else if(balance < -1 && this -> get_balance(node -> right) > 0) {
+		node -> right = this -> right_rotate(node -> right);
+		node = this -> left_rotate(node);
+	}
+
+	return result;
+}
