e7651a44dd6ecfbb76a02f54f29fcdb21c4ecafb
Tomas Baublys <tomas.baublys@mif.stud.vu.lt>
2025-10-10 11:27:44 +0300

    code formatting

--- include/min_heap.h
+++ include/min_heap.h
@@ -6,9 +6,6 @@
 #include <vector>
 #include "bits.h"
 
-
-
-
 class Min_heap{
     private:
     struct Heap_element{
@@ -22,19 +19,23 @@ class Min_heap{
     struct Compare_heap_element{
         bool operator()(Heap_element& a, Heap_element& b);
     };
-        std::priority_queue<Heap_element, std::vector<Heap_element>, Compare_heap_element> min_heap;
+
+    std::priority_queue<Heap_element, std::vector<Heap_element>, Compare_heap_element> min_heap;
 
     public:
         // push pop and other operations
-        
         uint16_t size();
+
         bool empty();
+
         const Heap_element& top();
+
         void push(Bits key, uint16_t level, uint16_t file_index);
+
         void pop();
+
         // for when we need to pop all of the elements with the same key
         void remove_by_key(Bits& key);
 };
-
 
 #endif // MIN_HEAP_H_INCLUDED
