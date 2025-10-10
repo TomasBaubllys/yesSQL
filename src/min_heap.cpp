3502db863e4d11f23d2616a42f34b19dddc0a22d
godagu <goda.gutparakyte@mif.stud.vu.lt>
2025-10-10 10:09:48 +0300

    added ehap files

---
+++ src/min_heap.cpp
@@ -0,0 +1,59 @@
+#include "../include/min_heap.h"
+\
+bool Compare_heap_element::operator()(Heap_element& a, Heap_element&b){
+    if(a.key != b.key){
+        return a.key > b.key;
+    }
+    
+    return a.level > b.level;   
+}
+
+Heap_element::Heap_element(Bits k, uint16_t l, uint16_t f_i)
+        : key(k), level(l), file_index(f_i){}
+
+
+uint16_t Min_heap:: size(){
+    return this -> min_heap.size();
+}
+
+bool Min_heap::empty(){
+    return this -> min_heap.empty();
+}
+
+const Heap_element& Min_heap:: top(){
+    return this -> min_heap.top();
+}
+
+
+void Min_heap::push(Bits key, uint16_t level, uint16_t file_index){
+    this -> min_heap.emplace(key, level, file_index);
+    return;
+}
+
+void Min_heap::pop(){
+    if(!(this -> min_heap.empty())){
+        min_heap.pop();
+    }
+
+    return;
+}
+// for when we need to pop all of the elements with the same key
+void Min_heap::remove_by_key(Bits& key){
+    if(this -> min_heap.empty()){
+        return;
+    }
+    while (true)
+    {
+        Heap_element element = this -> min_heap.top();
+        if(element.key == key){
+            this -> min_heap.pop();
+        }
+        else{
+            break;
+        }
+         
+    }
+
+    return;
+    
+}
