64dc083e85aec02fa57cd524e7b7a5b6eb4f474d
Tomas Baublys <tomas.baublys@mif.stud.vu.lt>
2025-10-12 23:06:23 +0300

    added server plan

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,5 +1,16 @@
 #include "../include/server.h"
 
+/* first create a all child servers (partitions)
+ * iniatialize main server
+ * wait for user requests
+ * redirect users request base on the index range
+ * return request from the main server 
+ * USER -> MAIN_SERVER -> partition1
+ *                      -> partition2
+ *                      -> ..........
+ *                      -> partitionN
+ */
+
 int main(int argc, char* argv[]) {
     Server server(8080);
     server.start();
