4a403b5d6598a85a081d2d78f1126f5d99452fc7
Tomas Baublys <tomas.baublys@mif.stud.vu.lt>
2025-10-16 10:57:56 +0300

    updated test script

--- lsm_tree/run_test_lsm.sh
+++ lsm_tree/run_test_lsm.sh
@@ -2,4 +2,6 @@
 
 g++ -o test_lsm -g src/lsm_tree.cpp src/crc32.cpp src/entry.cpp src/bits.cpp src/avl_tree.cpp src/mem_table.cpp src/min_heap.cpp tests/test_lsm.cpp src/ss_table.cpp src/ss_table_controller.cpp src/wal.cpp 
 
+rm -r ./data/val*
+
 ./test_lsm
