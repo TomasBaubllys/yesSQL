ece45b45dac83b05ad2936b581b6f9b36d1a3975
Tomas Baublys <tomas.baublys@mif.stud.vu.lt>
2025-10-25 12:02:33 +0300

    added docker compose file and moved some commands from start to constructor

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -14,37 +14,21 @@ Server::Server(uint32_t port) : port(port) {
 	if(setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &options, sizeof(options))) {
 		perror("setsockopt");
 	}
-
-
-}
-
-int8_t Server::start() {
-	// int32_t server_fd;
-	int32_t new_socket;
-	struct sockaddr_in address;
-	uint32_t address_length = sizeof(address);
-	// int32_t options = 1;
-
-	// const char* msg = "Hello from YSQL server\n";
-
-	// create socket
-	/*if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-		perror("Socket failed\n");
-		return -1;
-	}*/
-
-	// set socket options
-
-	// bind address to port
+  
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY;
 	address.sin_port = htons(port);
-
+  
 	if(bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
 		perror("bind failed");
-		return -1;
-	}
+    exit(-1);
+  }
+}
 
+int8_t Server::start() {
+	uint32_t new_socket;
+	uint32_t address_length = sizeof(this -> address);
+	const char* msg = "Hello from YSQL server\n";
 	if(listen(server_fd, 3) < 0) {
 		perror("listen");
 		return -1;
