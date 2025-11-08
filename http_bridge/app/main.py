c18413f05e063a747d95e44ed03407225fe47ce3
Goda Gutparakyte <goda.gutparakyte@mif.stud.vu.lt>
2025-11-09 01:38:32 +0200

    added fastAPI async pool for connection between http brisge and the main server. get, set, delete

--- http_bridge/app/main.py
+++ http_bridge/app/main.py
@@ -1,109 +1,55 @@
-import builder as bld
-import parser as prs
-import constants as cnt
-import socket
-import traceback
+
+from .tcp_pool import TCP_Connection_Pool
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from . import builder, parser
 
 HOST = "127.0.0.1"
 PORT = 8080
 
-try:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        print("Connecting to the main server...")
-        s.connect((HOST, PORT))
-        print("Connected!")
+app = FastAPI()
+pool = TCP_Connection_Pool(HOST, PORT, size=5)
 
-        s.sendall(bld.build_set_request("1", "Alio"))
-        data = s.recv(4096)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await pool.initialize()
+    print("TCP pool ready!")
 
-        try:
+    yield
+    await pool.close()
+    print("TCP pool cleaned")
 
-            parsed_response = prs.dispatch(data)
-            
-            print("Parsed:", parsed_response)
-
-        except Exception:
-            print("Error while parsing response!")
-            traceback.print_exc()  # prints full line number and error
-
-
-        # Send a GET request for key "1"
-        s.sendall(bld.build_get_request("1"))
-
-        # Receive the response
-        data = s.recv(4096)
-        try:
-
-            parsed_response = prs.dispatch(data)
-            
-            print("Parsed:", parsed_response)
-
-        except Exception:
-            print("Error while parsing response!")
-            traceback.print_exc()  # prints full line number and error
+app = FastAPI(lifespan=lifespan) 
+    
+@app.get("/get/{key}")
+async def get_value(key: str):
+    async with pool.get_connection() as conn:
+        msg = builder.build_get_request(key)
+        resp = await conn.send(msg)
+        parsed = parser.dispatch(resp)
+        return parsed
 
 
-        # Send a GET request for key "1"
-        s.sendall(bld.build_remove("1"))
-
-        # Receive the response
-        data = s.recv(4096)
-        try:
-
-            parsed_response = prs.dispatch(data)
-            
-            print("Parsed:", parsed_response)
-
-        except Exception:
-            print("Error while parsing response!")
-            traceback.print_exc()  # prints full line number and error
-except ConnectionRefusedError:
-    print("Failed to connect: is the server running?")
-except Exception as e:
-    print("Error:", e)
+@app.post("/set/{key}/{value}")
+async def set_value(key: str, value: str):
+    async with pool.get_connection() as conn:
+        msg = builder.build_set_request(key, value)
+        resp = await conn.send(msg)
+        parsed = parser.dispatch(resp)
+        return parsed
 
 
-"""
-msg = bld.build_get_request("foo")
+@app.delete("/delete/{key}")
+async def remove_value(key: str):
+    async with pool.get_connection() as conn:
+        msg = builder.build_remove(key)
+        resp = await conn.send(msg)
+        parsed = parser.dispatch(resp)
+        return parsed
+    
 
-print(msg)
-
-total_length, num_elements = struct.unpack_from("!QQ", msg, 0)
-command_number = struct.unpack_from("!H", msg, 16)[0]
-key_len = struct.unpack_from("!H", msg, 18)[0]
-key_bytes = msg[20:20+key_len]
-
-
-print(f"Total length: {total_length}")
-print(f"Num elements: {num_elements}")
-print(f"Command number: {command_number}")
-print(f"Key length: {key_len}")
-print(f"Key bytes: {key_bytes}")
-print(f"Key string: {key_bytes.decode()}")
-
-
-msg = bld.build_set_request("foo", "kaka")
-
-print(msg)
-
-total_length, num_elements = struct.unpack_from("!QQ", msg, 0)
-offset = 16
-command_number = struct.unpack_from("!H", msg, offset)[0]
-offset += cnt.COMMAND_LENGTH_COMMAND_MESSAGE
-key_len = struct.unpack_from("!H", msg, offset)[0]
-offset += cnt.COMMAND_LENGTH_KEY_LENGTH
-key_bytes = msg[offset:offset+key_len]
-offset += key_len
-value_len = struct.unpack_from("!I", msg, offset)[0]
-offset += cnt.COMMAND_LENGTH_VALUE_LENGTH
-value_bytes = msg[offset:offset+value_len]
-
-
-print(f"Total length: {total_length}")
-print(f"Num elements: {num_elements}")
-print(f"Command number: {command_number}")
-print(f"Key length: {key_len}")
-print(f"Key string: {key_bytes.decode()}")
-print(f"Value length: {value_len}")
-print(f"Value string: {value_bytes.decode()}")
-"""
+# TODO:
+# COMMAND_CODE_GET_KEYS = 4
+#COMMAND_CODE_GET_KEYS_PREFIX = 5
+#COMMAND_CODE_GET_FF = 6
+#COMMAND_CODE_GET_FB = 7
