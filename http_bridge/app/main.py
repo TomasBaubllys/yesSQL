18e09e56f4375bdecccb1cd60f25d839476e8095
Goda Gutparakyte <goda.gutparakyte@mif.stud.vu.lt>
2025-11-09 01:54:11 +0200

    changed the structure

--- http_bridge/app/main.py
+++ http_bridge/app/main.py
@@ -1,14 +1,13 @@
-
-from .tcp_pool import TCP_Connection_Pool
+from .tcp_instance_pool import pool
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from . import builder, parser
+from .routes import router
 
 HOST = "127.0.0.1"
 PORT = 8080
+POOL_SIZE = 5
 
 app = FastAPI()
-pool = TCP_Connection_Pool(HOST, PORT, size=5)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -20,32 +19,8 @@ async def lifespan(app: FastAPI):
     print("TCP pool cleaned")
 
 app = FastAPI(lifespan=lifespan) 
-    
-@app.get("/get/{key}")
-async def get_value(key: str):
-    async with pool.get_connection() as conn:
-        msg = builder.build_get_request(key)
-        resp = await conn.send(msg)
-        parsed = parser.dispatch(resp)
-        return parsed
 
-
-@app.post("/set/{key}/{value}")
-async def set_value(key: str, value: str):
-    async with pool.get_connection() as conn:
-        msg = builder.build_set_request(key, value)
-        resp = await conn.send(msg)
-        parsed = parser.dispatch(resp)
-        return parsed
-
-
-@app.delete("/delete/{key}")
-async def remove_value(key: str):
-    async with pool.get_connection() as conn:
-        msg = builder.build_remove(key)
-        resp = await conn.send(msg)
-        parsed = parser.dispatch(resp)
-        return parsed
+app.include_router(router)
     
 
 # TODO:
